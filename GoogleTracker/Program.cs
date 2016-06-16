26a3874cb206df895d2e48b246f08b6d70eb2fdd
Milosz Krajewski <miloszkrajewski@o2.pl>
2016-06-17 00:30:33 +0100

    refactoring

--- GoogleTracker/Program.cs
+++ GoogleTracker/Program.cs
@@ -1,24 +1,88 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace GoogleTracker
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var results = Query.GetSearchResults("Flying Colors");
-            using (var ctx = new ResultsStorage())
-            {
-                foreach (var result in results)
-                {
-                    ctx.Results.Add(result);
-                    ctx.SaveChanges();
-                }
-            }
-        }
-    }
+	public class SearchInstance
+	{
+		public DateTime Retreived { get; set; }
+		public string Query { get; set; }
+		public long Count { get; set; }
+	}
+
+	public class Result
+	{
+		public string Url { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public int SearchPageNo { get; set; }
+		public SearchInstance Instance { get; set; }
+	}
+
+	public static class Query
+	{
+		private const string GoogleQueryUrl = "http://google.co.uk/search?q=";
+
+		public static string SanitiseQuery(string query) { return Regex.Replace(query, "[ _]", "+"); }
+		private static long? TryParseLong(string text)
+		{
+			try { return long.Parse(text); } catch { return null; }
+		}
+
+		public static IEnumerable<Result> GetSearchResults(string query)
+		{
+			var document = new HtmlDocument();
+			var request = WebRequest.Create(GoogleQueryUrl + SanitiseQuery(query));
+			var response = request.GetResponse();
+			document.Load(response.GetResponseStream());
+			var instance = ExtractSearchInstance(document, query);
+			return document.DocumentNode
+				.SelectNodes(".//div[@class='g']")
+				.Select(r => ResultFromRaw(r, instance))
+				.Where(r => r != null);
+		}
+
+		private static SearchInstance ExtractSearchInstance(HtmlDocument document, string query)
+		{
+			var count = document.GetElementbyId("resultStats").InnerText.Split()
+				.Select(word => word.Replace(",", string.Empty))
+				.Select(TryParseLong)
+				.FirstOrDefault(c => c != null) ?? 0;
+			return new SearchInstance {
+				Retreived = DateTime.Now,
+				Query = query,
+				Count = count
+			};
+		}
+
+		private static Result ResultFromRaw(HtmlNode node, SearchInstance instance)
+		{
+			var title = node.SelectSingleNode(".//h3[@class='r']");
+			var url = node.SelectSingleNode(".//cite");
+			var description = node.SelectSingleNode(".//span[@class='st']");
+			return (title == null || url == null || description == null)
+				? null
+				: new Result {
+					Url = url.InnerText,
+					Title = title.InnerText,
+					Description = description.InnerText,
+					SearchPageNo = 1,
+					Instance = instance
+				};
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var results = Query.GetSearchResults("Flying Colors");
+			Console.WriteLine(results.Count());
+			Console.ReadLine();
+		}
+	}
 }
---
+++ GoogleTrackerFS/Program.fs
@@ -0,0 +1,46 @@
+﻿open System
+open System.Text.RegularExpressions
+open HtmlAgilityPack
+open System.Net
+
+let possibly f a = try f a |> Some with _ -> None
+let alternatively v o = defaultArg o v
+
+type SearchInstance = { 
+    Retreived: DateTime; Query: string; Count: int64 
+}
+
+type Result = { 
+    Url: string; Title: string; Description: string; SearchPageNo: int; Instance: SearchInstance 
+}
+
+let GoogleQueryUrl = "http://google.co.uk/search?q="
+
+let sanitiseQuery query = Regex.Replace(query, "[ _]", "+")
+
+let extractSearchInstance (query: string) (document: HtmlDocument) = 
+    let count = 
+        document.GetElementbyId("resultStats").InnerText.Split()
+        |> Seq.tryPick (fun w -> w.Replace(",", "") |> possibly int64)
+        |> alternatively 0L 
+    { Retreived = DateTime.Now; Query = query; Count = count }
+
+let resultFromRaw (instance: SearchInstance) (node: HtmlNode) =
+    let extract xp = node.SelectSingleNode(xp).InnerText
+    let url, title, description = 
+        extract ".//cite", extract ".//h3[@class='r']", extract ".//span[@class='st']"; 
+    { Url = url; Title = title; Description = description; SearchPageNo = 1; Instance = instance }
+
+let getSearchResults query =
+    let document = HtmlDocument()
+    WebRequest.Create(GoogleQueryUrl + (sanitiseQuery query)).GetResponse().GetResponseStream()
+    |> document.Load
+    let instance = document |> extractSearchInstance query
+    document.DocumentNode.SelectNodes(".//div[@class='g']")
+    |> Seq.choose (possibly (resultFromRaw instance))
+
+[<EntryPoint>]
+let main argv = 
+    "Flying Colors" |> getSearchResults |> Seq.length |> printfn "%d"
+    Console.ReadLine() |> ignore
+    0
